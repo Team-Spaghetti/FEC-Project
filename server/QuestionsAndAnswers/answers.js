1f5f50395d2e48caed3d2b6f7bb4ec3f79712da4
RasNewton <princegyekyeaddai@gmail.com>
2021-10-29 18:08:14 -0500

    routes complete

--- server/QuestionsAndAnswers/answers.js
+++ server/QuestionsAndAnswers/answers.js
@@ -6,26 +6,24 @@ var endPoint = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-atx/qa/answers'
 var auth = { headers: { 'Authorization': `${config}` }, params: { product_id: 1 } };
 
 routes.put(`/${1}/helpful`, (req, res) => {
-  axios.put(endPoint + `/${1}/helpful`, { answer_id: req.body.answer_id }, { headers: { 'Authorization': `${config}` } })
+  axios.put(endPoint + `/${1}/helpful`, { answer_id: req.body.answer_id }, auth)
     .then(response => {
-      console.log(response.data)
-      res.end();
+      res.status(200).send(response.statusText);
     })
     .catch(err => {
       console.error(err);
-      res.end();;
+      res.status(400).end();
     })
 })
 
 routes.put(`/${1}/report`, (req, res) => {
-  axios.put(endPoint + `/${1}/report`, { answer_id: req.body.answer_id }, { headers: { 'Authorization': `${config}` } })
+  axios.put(endPoint + `/${1}/report`, { answer_id: req.body.answer_id }, auth)
     .then(response => {
-      console.log(response.data)
-      res.end();
+      res.status(200).send(response.statusText);
     })
     .catch(err => {
       console.error(err);
-      res.end();;
+      res.status(400).end();
     })
 })
 
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var axios = require('axios');
+// var { urlencoded } = require('body-parser');
 var cors = require('cors');
-var { urlencoded } = require('body-parser');
 var app = express();
-var questions = require('./qtns&ans/questions');
-var answers = require('./qtns&ans/answers');
+var questions = require('./QuestionsAndAnswers/questions');
+var answers = require('./QuestionsAndAnswers/answers');
 
 app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 
 app.use('/qa/questions', questions);
 app.use('/qa/answers', answers);
@@ -18,4 +18,3 @@ var port = 3000;
 app.listen(port, () =>
   {console.log(`server is running and listening on http://localhost:${port}`)
 });
-
