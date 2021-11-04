85c3295e14018d3d8550283fe9992c0f3c159570
Miguel Regalado <miguelmobil21@gmail.com>
2021-11-04 16:54:39 -0500

    90% functionality

--- src/components/overview/overview.js
+++ src/components/overview/overview.js
@@ -3,14 +3,27 @@ import Images from './images.js'
 import ProductInfo from './productInfo.js'
 import StyleSelector from './styleSelector.js'
 import Cart from './cart.js'
+import Grid from '@mui/material/Grid'
 
 export default function Overview() {
   return(
     <div>
-      <Images />
-      <ProductInfo />
-      <StyleSelector />
-      <Cart />
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={8}>
+          <Images />
+        </Grid>
+        <Grid item xs={4} container spacing={2} direction="column">
+          <Grid item xs={1}>
+            <ProductInfo />
+          </Grid>
+          <Grid item xs={1}>
+            <StyleSelector />
+          </Grid>
+          <Grid item xs={1}>
+            <Cart />
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   )
 }
