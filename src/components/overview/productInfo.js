feef43d197428a5b4c7372c8f90f1a4db39db050
Miguel Regalado <miguelmobil21@gmail.com>
2021-11-10 10:48:32 -0600

    New share button functionality!

--- src/components/overview/productInfo.js
+++ src/components/overview/productInfo.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useProduct } from "../../contexts/ProductContext.js";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
+import {
+  FacebookShareButton,
+  PinterestShareButton,
+  TwitterShareButton,
+} from "react-share";
+import { FacebookIcon, TwitterIcon, PinterestIcon } from "react-share";
 
 export default function ProductInfo() {
   const { product, styleReducer, meta } = useProduct();
@@ -11,7 +14,12 @@ export default function ProductInfo() {
   return (
     <div className="product-info">
       <div>
-        <Rating value={meta.starRating || 0} precision={0.25} sx={{color:'purple'}} readOnly/>
+        <Rating
+          value={meta.starRating || 0}
+          precision={0.25}
+          sx={{ color: "purple" }}
+          readOnly
+        />
       </div>
       <div className="product-name">
         <p>{product.category}</p>
@@ -23,7 +31,9 @@ export default function ProductInfo() {
         <div className="prices">
           {styleValue.currentStyle && styleValue.currentStyle.sale_price ? (
             <div>
-              <p className='original-price'>${styleValue.currentStyle.original_price}</p>
+              <p className="original-price">
+                ${styleValue.currentStyle.original_price}
+              </p>
               <p className="sale-price">
                 ${styleValue.currentStyle.sale_price}
               </p>
@@ -36,9 +46,37 @@ export default function ProductInfo() {
         </div>
       </div>
       <div>
-        <a href="https://facebook.com"><FacebookIcon /></a>
-        <a href="https://twitter.com"><TwitterIcon /></a>
-        <a href="https://pinterest.com"><PinterestIcon /></a>
+        <FacebookShareButton
+          url={
+            styleValue.currentStyle
+              ? styleValue.currentStyle.photos[0].url
+              : "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+          }
+          quote={
+            "BROS AND BRODETTES: FEAST YOUR EYES ON THIS AMAZING PRODUCT!!!"
+          }
+        >
+          <FacebookIcon size={24} round={true}/>
+        </FacebookShareButton>
+        <TwitterShareButton
+          url={
+            styleValue.currentStyle
+              ? `http://www.urban-octo-chainsaw.com`
+              : "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+          }
+          title={`Hopefully, this doesn't offend you guys... `}
+        >
+          <TwitterIcon size={24} round={true}/>
+        </TwitterShareButton>
+        <PinterestShareButton
+        url={`http://www.urban-octo-chainsaw.com`}
+        media={
+          styleValue.currentStyle
+            ? styleValue.currentStyle.photos[0].url
+            : "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+        }>
+          <PinterestIcon size={24} round={true}/>
+        </PinterestShareButton>
       </div>
     </div>
   );
